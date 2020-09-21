1e467cf75b12e73ec78cc8ad0712f6fd1b882f5b
helloalexdodd <hello@alexdodd.ca>
2020-09-20 21:48:17 -0400

    return accounts alphabetically

--- server/lib/controllers/accounts/get-all-accounts.js
+++ server/lib/controllers/accounts/get-all-accounts.js
@@ -2,12 +2,21 @@ const { User } = require('models/user');
 
 const getAccounts = async (req, res) => {
   const { _id } = req.user;
-  const user = await User.findOne({ _id }).populate('accounts');
+  const user = await User.findOne({ _id })
+    .populate('accounts')
+    .sort({ accounts: 1 });
   if (!user.accounts) return res.status(404).send('No transactions found.');
 
-  const accounts = user.accounts.map((account) => {
-    return { name: account.name, _id: account._id };
-  });
+  const accounts = user.accounts
+    .map((account) => ({
+      name: account.name,
+      _id: account._id,
+    }))
+    .sort((a, b) => {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+      return 1;
+    });
+
   res.send(accounts);
 };
 
