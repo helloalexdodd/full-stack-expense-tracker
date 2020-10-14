e8889adfac25671d45f390778280265add904651
helloalexdodd <hello@alexdodd.ca>
2020-10-14 14:43:09 -0400

    changed to mongodb atlas database

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const config = require('config');
 const { logger } = require('logger');
 
-module.exports = () => {
+module.exports = async () => {
   const db = config.get('db');
-  mongoose
-    .connect(db, {
+
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
-    })
-    .then(() => logger.info(`Connected to ${db}...`));
+    });
+    if (process.env.NODE_ENV !== 'production') {
+      logger.info(`Connected to ${db}...`);
+    } else {
+      logger.info(`Connected to database...`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
--- server/config/production.js
+++ server/config/production.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 5000,
   jwtPrivateKey: 'super_secret_key',
-  db: 'mongodb://localhost/expense-tracker',
+  db: process.env.DB,
   emailAddress: process.env.EMAIL_ADDRESS || '',
   emailPassword: process.env.EMAIL_PASSWORD || '',
-  baseUrl: 'http://localhost:8080/#',
+  baseUrl: process.env.BASE_URL || 'http://localhost:8080/#',
 };
